c9f4d8c46609fec773a62dbb1957cfb3b713a89a
ldad621 <ldad621@aucklanduni.ac.nz>
2017-06-06 19:24:17 +1200

    Add files via upload

---
+++ Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// using UnityEngine;
+
+class Assignment /*ldad621- making the class public to get data from table into Unity */
+{
+	public string RowID { get; set;}
+	public string TimeCreated { get; set; }
+	public string CardName { get; set; }
+	public string ListName { get; set; }
+	public string BoardName { get; set; } 
+	public string CreatorName {get; set; }
+	public string CreatorUsername { get; set; }
+	public string BoardURL {get; set; } 
+} 
