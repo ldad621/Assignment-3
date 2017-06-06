c26d17cf78454c17cb20b6cda327d80706378e19
ldad621 <ldad621@aucklanduni.ac.nz>
2017-06-06 13:01:23 +1200

    FINAL
    Final working product

--- Sketch.cs
+++ Sketch.cs
@@ -1,51 +1,45 @@
 using UnityEngine;
-using Pathfinding.Serialization.JsonFx; //make sure you include this using
+using Pathfinding.Serialization.JsonFx;
 
 public class Sketch : MonoBehaviour {
-    public GameObject myPrefab;
-    //string DataSeat = "treesurveyv3"; 
-    // Put your URL here
-    //public 
-    string _WebsiteURL = "http://infomgmt192.azurewebsites.net/tables/" + "treesurveyv3" + "?zumo-api-version=2.0.0";
+	public GameObject myPrefab;
 
-    void Start () {
-        //Reguest.GET can be called passing in your ODATA url as a string in the form:
-        //http://{Your Site Name}.azurewebsites.net/tables/{Your Table Name}?zumo-api-version=2.0.0
-        //The response produce is a JSON string
-//        string Dataseat = "treesurveyv3";
-        string jsonResponse = Request.GET(_WebsiteURL);
+	string _WebsiteURL = "http://ldad621.azurewebsites.net/tables/ass3?zumo-api-version=2.0.0"; /*ldad621- azure website- table from swagger uploaded */
 
-        //Just in case something went wrong with the request we check the reponse and exit if there is no response.
-        if (string.IsNullOrEmpty(jsonResponse))
-        {
-            return;
-        }
+	void Start () {
 
-        //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
-        TreeSurvey[] treesurveys = JsonReader.Deserialize<TreeSurvey[]>(jsonResponse);
+		string jsonResponse = Request.GET(_WebsiteURL);
 
-        int totalCubes = treesurveys.Length;
-        int totalDistance = 5;
-        int i = 0;
-        //We can now loop through the array of objects and access each object individually
-        foreach (TreeSurvey treesurvey in treesurveys)
-        {
-            //Example of how to use the object
-            Debug.Log("This products name is: " + treesurvey.Location);
-            float perc = i / (float)totalCubes;
-            i++;
-            float x = perc * totalDistance;
-            float y = treesurvey.Y;// 5.0f;
-            float z = 0.0f;
-            GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-            newCube.GetComponent<myCubeScript>().setSize((1.0f - perc) * 2);
-            newCube.GetComponent<myCubeScript>().ratateSpeed = perc;
-            newCube.GetComponentInChildren<TextMesh>().text = treesurvey.Location;
-        }
+		//Just in case something went wrong with the request we check the reponse and exit if there is no response.
+		if (string.IsNullOrEmpty(jsonResponse))
+		{
+			return;
+		}
+
+		//We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
+		Assignment[] Ass3 = JsonReader.Deserialize<Assignment[]>(jsonResponse);
+
+		int totalCubes = Ass3.Length; /*ldad621- definition for length not working at first */ 
+		int totalDistance = 5;
+		int i = 0;
+
+		/*ldad621- for loop begins here -  */
+		foreach (Assignment ass3 in Ass3)
+		{
+			Debug.Log("This list name is: " + ass3.CardName); /*ldd621 extracting the data from Table "ASS3"- shown at bottom of game screen */
+			float perc = i / (float)totalCubes;
+			i++;
+			float x = perc * totalDistance;
+			float y = 5.0f; /*ldad621- position of the cube in game mode on unity */
+			float z = 0.0f;
+			GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+			newCube.GetComponent<myCubeScript>().setSize((1.0f - perc) * 2);
+			newCube.GetComponent<myCubeScript>().ratateSpeed = perc; /*ldad621- creates boxes for each row in my excel table */
+			newCube.GetComponentInChildren<TextMesh>().text = ass3.CardName; /*ldad621- Extracting data from cardname */
+		}
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+
 	}
-}
+} 
